f77c20c15e4d45f90d07f29a21373e8cc02da026
Esteban Beltran <esteban.beltran@seidor.com>
2026-05-24 18:42:28 -0400

    add: logs

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -13,6 +13,7 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID || "dummy-app-id"
 };
 
+console.log('firebaseConfig', firebaseConfig);
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
